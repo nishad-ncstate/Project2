985d2bd2f33017da353799df3263183db0964551
Nishad Waghmare <nwaghma@ncsu.edu>
2024-07-08 20:16:13 -0400

    bandsintown app

--- Project2/app.R
+++ Project2/app.R
@@ -1,51 +1,72 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Bandsintown API Data Explorer"),
+  tabsetPanel(
+    tabPanel("About", includeMarkdown("about.md")),
+    tabPanel("Data Download", 
+             sidebarLayout(
+               sidebarPanel(
+                 textInput("artist_name", "Artist Name:", value = "Coldplay"),
+                 actionButton("query", "Query API")
+               ),
+               mainPanel(
+                 tableOutput("api_data")
+               )
+             )
+    ),
+    tabPanel("Data Exploration", 
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("x_var", "X-axis Variable:", choices = NULL),
+                 selectInput("y_var", "Y-axis Variable:", choices = NULL),
+                 selectInput("plot_type", "Plot Type:", choices = c("Scatter Plot", "Bar Plot")),
+                 actionButton("plot", "Generate Plot")
+               ),
+               mainPanel(
+                 plotOutput("data_plot")
+               )
+             )
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  observeEvent(input$query, {
+    # Construct the API endpoint URL using the input artist name
+    endpoint <- paste0("https://rest.bandsintown.com/artists/", input$artist_name, "/events?app_id=test")
+    
+    # Make the GET request to the API
+    response <- GET(endpoint)
+    
+    # Parse the JSON response into a list
+    data <- fromJSON(content(response, "text"), flatten = TRUE)
+    
+    # Convert the list to a data frame
+    df <- as.data.frame(data)
+    
+    # Display the data frame in the table output
+    output$api_data <- renderTable({ df })
+    
+    # Update the select inputs for plotting based on the column names of the data frame
+    updateSelectInput(session, "x_var", choices = names(df))
+    updateSelectInput(session, "y_var", choices = names(df))
+  })
+  
+  observeEvent(input$plot, {
+    req(input$x_var, input$y_var, input$plot_type)
+    plot <- ggplot(df, aes_string(x = input$x_var, y = input$y_var))
+    if (input$plot_type == "Scatter Plot") {
+      plot <- plot + geom_point()
+    } else if (input$plot_type == "Bar Plot") {
+      plot <- plot + geom_bar(stat = "identity")
+    }
+    output$data_plot <- renderPlot({ plot })
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
